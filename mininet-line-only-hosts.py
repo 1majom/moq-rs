2a5a31af0d8bd51635fb2affea706380647b48cb
Balazs Szenczy <szenczy.balazs@pm.me>
2024-07-05 11:39:25 +0200

    correcint local.sh, adding sslkeylog, changing py after exp

--- mininet-line-only-hosts.py
+++ mininet-line-only-hosts.py
@@ -13,24 +13,23 @@ from mininet.node import Node
 #!/usr/bin/env python
 
 """
-- cmds for 2 relay scenario, !: the /dev/api script is not in the repo, it is changed to fit the current implementation
   +---------+       +------------+   +------------+   +---------+
-  | h1(pub) +-------+ h2(relay1) +---+ h3(relay2) +---+ h4(sub) |
+  | h1(pub) +------>+ h2(relay1) +-->+ h3(relay2) +-->+ h4(sub) |
   +---------+       +------------+   +------------+   +---------+
                    |       |
                    |       |
                    |       |
-                +-----+    |    +----+
-                |  h5 |----+    |root|
-                |(api)+---------+----+
+                +-----+    |    +---------------------+
+                |  h5 |----+    |root(redis container)|
+                |(api)+---------+---------------------+
                 +-----+
-
-    - h5 ./dev/api &
+- the /dev/api script is not in the original repo, it is changed to fit the current implementation
+- these would be commands for 2 relays, 1 publisher and 1 subscriber, but for some reason if ran from the mininet cli it wont work as expected
+    - h5 REDIS=10.0.6.99 ./dev/api &
     - h2 'RUST_LOG=debug RUST_BACKTRACE=1 ./target/debug/moq-relay --bind '10.0.2.1:4443' --api http://10.0.5.1 --node 'https://10.0.2.1:4443' --tls-cert dev/localhost.crt --tls-key dev/localhost.key --tls-disable-verify --dev &'
     - h3 'RUST_LOG=debug RUST_BACKTRACE=1 ./target/debug/moq-relay --bind '10.0.2.2:4443' --api http://10.0.4.2 --node 'https://10.0.2.2:4443' --tls-cert dev/localhost.crt --tls-key dev/localhost.key --tls-disable-verify --dev &'
     - h1 ffmpeg -hide_banner -stream_loop -1 -re -i ./dev/bbb.mp4 -c copy -an -f mp4 -movflags cmaf+separate_moof+delay_moov+skip_trailer+frag_every_frame - |  RUST_LOG=debug RUST_BACKTRACE=1 ./target/debug/moq-pub --name bbb https://10.0.2.1:4443 --tls-disable-verify &
     - h4 RUST_LOG=debug RUST_BACKTRACE=1 ./target/debug/moq-sub --name bbb https://10.0.2.2:4443 --tls-disable-verify | ffplay -
-- when running into problems wait and dont forget to use sudo mn -c
 """
 
 import re
@@ -46,6 +45,14 @@ from mininet.log import info
 from mininet.node import Node
 
 from mininet.link import TCLink
+import subprocess
+import os
+
+# Running cleanup before starting the mininet
+if not os.geteuid() == 0:
+    exit("This script must be run as root")
+subprocess.call(['sudo', 'mn', '-c'])
+
 
 
 if __name__ == '__main__':
@@ -69,11 +76,11 @@ if __name__ == '__main__':
     net = Mininet( topo=None, waitConnected=True, link=partial(TCLink) )
     net.staticArp()
     switch = net.addSwitch('s1',failMode='standalone')
-    h1=net.addHost('h1', mac='00:00:00:00:01:01')
-    h2=net.addHost('h2', mac='00:00:00:00:02:01')
-    h3=net.addHost('h3',ip='10.0.2.2/24', mac='00:00:00:00:03:01')
-    h4=net.addHost('h4', ip='10.0.3.2/24', mac='00:00:00:00:04:01')
-    h5=net.addHost('h5', ip='10.0.5.1/24', mac='00:00:00:00:05:01')
+    h1=net.addHost('h1')
+    h2=net.addHost('h2')
+    h3=net.addHost('h3',ip='10.0.2.2/24')
+    h4=net.addHost('h4', ip='10.0.3.2/24')
+    h5=net.addHost('h5', ip='10.0.5.1/24')
 
     a=net.addLink(h1,h2)
     b=net.addLink(h2,h3,
@@ -101,22 +108,7 @@ if __name__ == '__main__':
 
     net.start()
 
-    h1.cmd('ip link set h1-eth0 address 00:00:00:00:01:01')
-
-    h2.cmd('ip link set h2-eth0 address 00:00:00:00:02:01')
-    h2.cmd('ip link set h2-eth1 address 00:00:00:00:02:02')
-    h2.cmd('ip link set h2-eth2 address 00:00:00:00:02:03')
-
-    h3.cmd('ip link set h3-eth1 address 00:00:00:00:03:02')
-    h3.cmd('ip link set h3-eth2 address 00:00:00:00:03:03')
-
-    h4.cmd('ip link set h4-eth0 address 00:00:00:00:04:01')
-
-    h5.cmd('ip link set h5-eth0 address 00:00:00:00:05:01')
-    h5.cmd('ip link set h5-eth1 address 00:00:00:00:05:02')
-    h5.cmd('ip link set h5-eth2 address 00:00:00:00:05:03')
-
-
+    # there were some lines making sure there are no arp traffic while streaming, but they are not needed
 
     h2.cmd('ip addr add 10.0.2.1/24 dev h2-eth1')
     h2.cmd('ip addr add 10.0.5.2/24 dev h2-eth2')
@@ -131,54 +123,38 @@ if __name__ == '__main__':
     h1.cmd('ip route add 10.0.2.0/24 via 10.0.0.2')
     h4.cmd('ip route add 10.0.2.0/24 via 10.0.3.1')
 
-    h3.cmd('arp -s 10.0.3.2 00:00:00:00:04:01')
-    h4.cmd('arp -s 10.0.3.1 00:00:00:00:03:02')
-    h2.cmd('arp -s 10.0.0.1 00:00:00:00:01:01')
-
-    h3.cmd('arp -s 10.0.2.1 00:00:00:00:02:02')
-    h2.cmd('arp -s 10.0.2.2 00:00:00:00:03:01')
-    h1.cmd('arp -s 10.0.0.2 00:00:00:00:02:01')
-
-
-
-
-    # h2.cmd('arp -s 10.0.0.1 00:00:00:00:01:01')
-    # h1.cmd('arp -s 10.0.2.1 00:00:00:00:02:02')
-    # h1.cmd('arp -s 10.0.3.1 00:00:00:00:03:02')
-    # h1.cmd('arp -s 10.0.4.2 00:00:00:00:05:05')
-
-    # # # Add sticky ARP entries to h4
-    # h4.cmd('arp -s 10.0.0.1 00:00:00:00:01:01')
-    # h4.cmd('arp -s 10.0.2.1 00:00:00:00:02:02')
-    # h4.cmd('arp -s 10.0.3.1 00:00:00:00:03:02')
-    # h4.cmd('arp -s 10.0.5.2 00:00:00:00:02:03')
-
-
 
     print( "Dumping host connections" )
     dumpNodeConnections(net.hosts)
 
-    h5.cmd('REDIS=10.0.6.99 ./dev/api &')
-    h2.cmd('RUST_LOG=debug RUST_BACKTRACE=1 ./target/debug/moq-relay --bind \'10.0.2.1:4443\' --api http://10.0.5.1 --node \'https://10.0.2.1:4443\' --tls-cert ./dev/localhost.crt --tls-key ./dev/localhost.key --tls-disable-verify --dev &')
-    h3.cmd('RUST_LOG=debug RUST_BACKTRACE=1 ./target/debug/moq-relay --bind \'10.0.2.2:4443\' --api http://10.0.4.2 --node \'https://10.0.2.2:4443\' --tls-cert ./dev/localhost.crt --tls-key ./dev/localhost.key --tls-disable-verify --dev &')
-    sleep(0.3)
+    h5.cmd('REDIS=10.0.6.99 ./dev/api > /tmp/out-h5 &')
+    template_for_relays = (
+        'RUST_LOG=debug RUST_BACKTRACE=0 SSLKEYLOGFILE=./ssl/{host} '
+        './target/debug/moq-relay --bind \'{bind}\' --api {api} --node \'{node}\' '
+        '--tls-cert ./dev/localhost.crt --tls-key ./dev/localhost.key '
+        '--tls-disable-verify --dev > /tmp/out-{host} &'
+    )
 
-    h1.cmd('ffmpeg -hide_banner -stream_loop -1 -re -i ./dev/bbb.mp4 -c copy -an -f mp4 -movflags cmaf+separate_moof+delay_moov+skip_trailer+frag_every_frame - |  RUST_LOG=debug RUST_BACKTRACE=1 ./target/debug/moq-pub --name bbb https://10.0.2.1:4443 --tls-disable-verify &')
-    sleep(0.5)
+    h2.cmd(template_for_relays.format(
+        host='h2', bind='10.0.2.1:4443', api='http://10.0.5.1', node='https://10.0.2.1:4443'
+    ))
+    sleep(0.7)
 
-    # h1.cmd("wireshark --interface h1-eth0 -k &")
-    # # h4.cmd("wireshark --interface h4-eth0 -k &")
-    # # h3.cmd("wireshark --interface h3-eth0 -k &")
-    # # h2.cmd("wireshark --interface h2-eth1 -k &")
-
-    h1.cmd("tshark -i h1-eth0 -w ./h1-eth0.pcap &")
-    h4.cmd("tshark -i h4-eth0 -w ./h4-eth0.pcap &")
-    h3.cmd("tshark -i h3-eth0 -w ./h3-eth0.pcap &")
-    h2.cmd("tshark -i h2-eth1 -w ./h2-eth1.pcap &")
+    h3.cmd(template_for_relays.format(
+        host='h3', bind='10.0.2.2:4443', api='http://10.0.4.2', node='https://10.0.2.2:4443'
+    ))
+    sleep(0.7)
 
 
-    # h4.cmd('RUST_LOG=debug RUST_BACKTRACE=1 ./target/debug/moq-sub --name bbb https://10.0.2.2:4443 --tls-disable-verify | ffplay -')
-    # h4 RUST_LOG=debug RUST_BACKTRACE=1 ./target/debug/moq-sub --name bbb https://10.0.2.2:4443 --tls-disable-verify | ffplay -x 200 -y 100 -
+	# for troubleshooting
+    for h in [h1, h2, h3, h4]:
+        h.cmd(f"tshark -i any -w ./{h.name}.pcap &")
+
+	# Sadly these two commands have to be run manually in xterm windows started by xterm h1 h4 because for some reason otherwise the stream would stop after 90ish seconds
+	# h1
+	# ffmpeg -hide_banner -stream_loop -1 -re -i ./dev/bbb.mp4 -c copy -an -f mp4 -movflags cmaf+separate_moof+delay_moov+skip_trailer+frag_every_frame - |   SSLKEYLOGFILE=./ssl/h1 RUST_LOG=info RUST_BACKTRACE=1 ./target/debug/moq-pub --name bbb https://10.0.2.1:4443 --tls-disable-verify > /tmp/out-h1
+    # h4
+	# RUST_LOG=debug RUST_BACKTRACE=1 SSLKEYLOGFILE=./ssl/h4  ./target/debug/moq-sub --name bbb https://10.0.2.2:4443 --tls-disable-verify | ffplay -x 200 -y 100 -
 
 
 
